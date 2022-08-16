d00cc76ff743299b4474f5694fdac0d92ec6cdb6
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-08-16 19:26:44 +0900

    로그인 UI

--- app/src/main/java/com/modoohealing/modo/LoginActivity.kt
+++ app/src/main/java/com/modoohealing/modo/LoginActivity.kt
@@ -5,12 +5,22 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.modoohealing.modo.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
 
     lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
     }
 }
