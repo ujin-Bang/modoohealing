9015b29fe0a38a72934e6d50d6363c77fc2afcaf
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-08-17 11:24:15 +0900

    버튼 클릭시 ripple efect(누름효과) 적용

--- app/src/main/java/com/modoohealing/modo/LoginActivity.kt
+++ app/src/main/java/com/modoohealing/modo/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.modoohealing.modo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.bind
+import androidx.databinding.DataBindingUtil.setContentView
 import com.modoohealing.modo.databinding.ActivityLoginBinding
 
 class LoginActivity : BaseActivity() {
@@ -18,7 +21,23 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        binding.btnKakaoLogin.setOnClickListener {
+            Toast.makeText(mContext, "카톡로그인 눌림", Toast.LENGTH_SHORT).show()
+        }
 
+        binding.btnGoogleLogin.setOnClickListener {
+            Toast.makeText(mContext, "구글로그인 눌림", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnSignUp.setOnClickListener {
+            Toast.makeText(mContext, "간편 회원가입 클릭", Toast.LENGTH_SHORT).show()
+        }
+        binding.btnNaverLogin.setOnClickListener {
+            Toast.makeText(mContext, "네이버 로그인클릭", Toast.LENGTH_SHORT).show()
+        }
+        binding.btnEmailLogin.setOnClickListener {
+            Toast.makeText(mContext, "이메일 로그인 클릭", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun setValues() {
