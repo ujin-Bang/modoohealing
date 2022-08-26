d05c79fe467e9a2211c391cf8edeb4047334edae
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-08-26 13:47:37 +0900

    메인화면 바텀네비게이션 아이템에 할당될 4개의 프래먼트 작업 - 해당 아이템 클릭시 화면안나오는 오류 -> constraint위치속성 확인으로 해결

--- app/src/main/java/com/modoohealing/modo/MainActivity.kt
+++ app/src/main/java/com/modoohealing/modo/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.modoohealing.modo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.modoohealing.modo.bottomhome.HomeFragment
+import com.modoohealing.modo.bottommore.SeeMoreFragment
+import com.modoohealing.modo.bottommypage.MyPageFragment
+import com.modoohealing.modo.bottomusers.UsersFragment
 import com.modoohealing.modo.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
     lateinit var binding: ActivityMainBinding
+    private val homeFragment by lazy { HomeFragment() }
+    private val usersFragment by lazy { UsersFragment() }
+    private val myPageFragment by lazy { MyPageFragment() }
+    private val seeMoreFragment by lazy { SeeMoreFragment() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
@@ -19,5 +30,35 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+        initBottomNavigationBar()
+        changeFragment(homeFragment)
+
     }
+    private fun initBottomNavigationBar(){
+        binding.mainBottomNaivigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> {
+                    changeFragment(homeFragment)
+                }
+                R.id.users ->{
+                    changeFragment(usersFragment)
+                }
+                R.id.myPage ->{
+                    changeFragment(myPageFragment)
+                }
+                R.id.seeMore ->{
+                    changeFragment(seeMoreFragment)
+                }
+            }
+            true
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .apply { replace(R.id.mainFragmentContainer, fragment)
+                commit()
+            }
+    }
+
 }
